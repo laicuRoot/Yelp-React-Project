c82a11c5c1148a7d08fc72bf1324aa4d9e07c481
laicuRoot <jdblancoes@gmail.com>
2020-07-03 15:45:43 +0100

    removed comment

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -34,14 +34,4 @@ export class App extends React.Component{
   }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>ravenous</h1>
-//       <SearchBar />
-//       <BusinessList businesses={businesses}/>
-//     </div>
-//   );
-// }
-
 export default App;
--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -1,32 +1,30 @@
 const apiKey = 'VKBV-z1BQ2NLLdnzrFo8zFb8GeaazmHXn8qLcTHJe9ZbRd9ahUth66RrKRRQ6mJfqbfk2Atj0crwuNJVSsy3zbbeMxMDEdkvJidlxdLtsxsKFmAU9j66za7VNxX_XnYx';
 
 const Yelp = {
-  searchYelp(term, location, sortBy){
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
-    {
-      headers: { 
-      Authorization: `Bearer ${apiKey}`
-    },
-  }).then((response)=> {
-      return response.json();
-    }).then((jsonResponse)=>{
-      if(jsonResponse.businesses){
-        return jsonResponse.businesses.map(((business)=>{
-          return {
-            id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            address: business.location.address1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories[0].title,
-            rating: business.rating,
-            reviewCount: business.review_count,
-          }
-        }));
-      }
-    })
+  async searchYelp(term, location, sortBy){
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        },
+      });
+    const jsonResponse = await response.json();
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map(((business) => {
+        return {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count,
+        };
+      }));
+    }
   }
 };
 
